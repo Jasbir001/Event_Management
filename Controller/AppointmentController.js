4e198a2146d72098f622ecd5a40c377328de96fa
Anchu749 <jasbirverma441441@gmail.com>
2026-01-01 12:57:34 +0530

    Event add booking

--- Controller/AppointmentController.js
+++ Controller/AppointmentController.js
@@ -1,4 +1,5 @@
 const appointmentModal = require('../Modal/Appointment')
+
 class AppointmentController{
     Add_appointment(req, res){
      
@@ -13,13 +14,31 @@ class AppointmentController{
             }
             appointmentModal.create(data,(err)=>{
                 if(err){
-                    res.render('Home',{msg:"Error in Booking Appointment"})
+                    res.render('Home',{msg:"Error in Book Appointment"})
                 }
                 else{
                     res.render('Home',{msg:data.Name +" Appointment Booked Successfully"})
                 }
             })
         }
+    Add_booking(req,res){
+        const data = {
+            Name:req.body.name,
+            Email:req.body.email,
+            Phone:req.body.phone,
+            Time:req.body.time,
+            Type:req.body.type,
+            ApxSize:req.body.apxsize
+        }
+        appointmentModal.create(data,(err)=>{
+                if(err){
+                    res.render('eventbooked',{msg:"Error In Booking event!! Try Again"})
+                }
+                else{
+                    res.render('eventbooked',{msg:data.Name +"Event is Booked Successfully"})
+                }
+            })
+    }
 }
 
 
